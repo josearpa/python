e67a74e453280890dc5258c314d91136ad7f85ea
Jose Aristizabal <jaristiz@oc8115247748.ibm.com>
2018-10-07 18:40:27 -0500

    Se agrega codigo para manejo de variables de numero y string

---
+++ variables-match.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+a = 2
+b = 3
+c = 5
+resultado = 0
+#print "a=" (a)
+print "=========================\na=" + str(a)
+print "b=" + str(b)
+print "c=" + str(c)
+print "resultado=" + str(resultado) + "\n========================="
+suma1= a + b
+print "a+b=" + str(suma1)
+mul1= c * a
+print "c*a=" + str(mul1)
+suma2=(b + c) - a
+print "(b+c)-a=" +str(suma2)
+division=c / a
+print "c/a="+str(division)
+modulo=c // b
+print "c//b="+str(modulo)
+potencia=a ** b
+print "a**b="+str(potencia)
+mul2=(a + b + c) * 2
+print "(a+b+c)*2="+str(mul2)
+nose=c % b
+print "c%b="+str(nose)
